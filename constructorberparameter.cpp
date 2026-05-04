844a5db7710dbd98100030bb39ccdf258100fc97
haifaamalinawahdan <haifanew3@gmail.com>
2026-05-04 13:10:17 +0700

    memanggil constructor berparameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,11 @@ public :
     mahasiswa(int nim, string nama); //Constructor dg parameter
 };
 
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : "<< nama << endl;
+}
+
+
